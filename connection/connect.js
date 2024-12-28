ff70fdfc9739f0ea7cad0940061382b02b539496
HritikDev <hritikdev24@gmail.com>
2024-12-28 17:40:51 +0530

    version

--- connection/connect.js
+++ connection/connect.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
-async function dbConnect(collection) {
-  return await mongoose.connect(`mongodb+srv://hritikGangadhar:Hritik@11@cluster0.rgkrs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0${collection}`);
+async function dbConnect(databaseName) {
+  const uri = `mongodb+srv://hritikGangadhar:Hritik@11@cluster0.rgkrs.mongodb.net/${databaseName}?retryWrites=true&w=majority&appName=Cluster0`;
+  
+  try {
+    await mongoose.connect(uri);
+    console.log(`Connected to database: ${databaseName}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 }
 
-
-module.exports={dbConnect};
+module.exports = { dbConnect };
