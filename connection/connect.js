3fb0acba9ac3b9ece58662a5b02e57efb25a14b9
HritikDev <hritikdev24@gmail.com>
2024-12-28 17:31:50 +0530

    then

--- connection/connect.js
+++ connection/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function dbConnect(collection) {
-  return await mongoose.connect(`mongodb+srv://hritikdev24:Hritik@11@cluster0.rgkrs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/${collection}`);
+  return await mongoose.connect(`mongodb+srv://hritikGangadhar:Hritik@11@cluster0.rgkrs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0${collection}`);
 }
 
 
